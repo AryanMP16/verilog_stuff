92a782fc39c9e1b6e5be1f03c10b60a98f0052e0
AryanMP16 <43282996+AryanMP16@users.noreply.github.com>
2024-12-16 16:03:20 -0800

    added clock

--- PT1_CPU/clock.v
+++ PT1_CPU/clock.v
@@ -1,8 +1,22 @@
-module clock (output signal);
-	reg clk;
-	assign signal = 0;
+module clock (output reg signal);
 	initial begin
-		assign clk = signal;
-		forever #5 clk = ~clk;
+		signal = 0;
+		forever #5 signal = ~signal;
 	end
 endmodule : clock
+
+module clock_test;
+	wire signal;
+	integer count;
+
+	clock c(.signal(signal));
+	initial begin
+		$display("Clock signal: ");
+		#1;
+		for (count = 0; count < 100; count = count + 1) begin
+			$monitor("%b", signal);
+			#5;
+		end
+		$finish;
+	end
+endmodule : clock_test
