b1c28caa92eb01248e8c9ff8ed229352d958864e
AryanMP16 <43282996+AryanMP16@users.noreply.github.com>
2024-12-17 17:37:39 -0800

    testbench for MUX. successful tests

--- PT1_CPU/register_file.v
+++ PT1_CPU/register_file.v
@@ -1,1 +1,2 @@
-module register_file (input mode/*if 1, RT; otherwise read data*/, input [4:0] read_address
+//module register_file (input [5 : 0] read_address/*if msb = 1, read data; otherwise RT*/, input [4 : 0] write_address, input [15 : 0] data_in);
+	
